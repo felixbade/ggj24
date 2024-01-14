cec5c857e6446a99ec0d4b9807256a7c0cb62c70
Felix Bade <flix.bade@gmail.com>
2024-01-14 16:37:17 +0100

    Add pixi rendering + controller

--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,18 @@
+import { app, container } from './render.js'
+const { controller, InputType } = UniversalGameController;
+
 window.addEventListener('load', () => {
-    const canvasContainer = document.getElementById('canvas-container')
-    const fsButton = document.getElementById('full-screen-button')
-    if (canvasContainer.requestFullscreen) {
-        fsButton.addEventListener('click', () => {
-            canvasContainer.requestFullscreen()
-        })
-    } else {
-        fsButton.style.display = 'none'
-    }
+
+    const gameContainer = new PIXI.Container()
+    container.addChild(gameContainer)
+
+    const particle = PIXI.Sprite.from('assets/images/gas-particle.svg')
+    gameContainer.addChild(particle);
+
+    app.ticker.add(delta => {
+        const speed = 5;
+        particle.x += controller.move.x * delta * speed;
+        particle.y += controller.move.y * delta * speed;
+    });
+
 });
