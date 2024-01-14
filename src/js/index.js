08fd942e4099f09fdbc006dec6b557ee81f06950
Felix Bade <flix.bade@gmail.com>
2024-01-14 16:19:04 +0100

    Add boilerplate for full screen canvas

---
+++ src/js/index.js
@@ -0,0 +1,11 @@
+window.addEventListener('load', () => {
+    const canvasContainer = document.getElementById('canvas-container')
+    const fsButton = document.getElementById('full-screen-button')
+    if (canvasContainer.requestFullscreen) {
+        fsButton.addEventListener('click', () => {
+            canvasContainer.requestFullscreen()
+        })
+    } else {
+        fsButton.style.display = 'none'
+    }
+});
