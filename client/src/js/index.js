1903f68cfacde087899d2e975788df3f21820428
Felix Bade <flix.bade@gmail.com>
2024-01-26 22:02:16 +0200

    Add acceleration + spaceship rotation

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -11,18 +11,29 @@ window.addEventListener('load', () => {
     const particle = PIXI.Sprite.from('assets/images/ship-1.png');
     particle.anchor.set(0.5);
     particle.scale.set(0.05);
+    particle.vx = 0;
+    particle.vy = 0;
     gameContainer.addChild(particle);
 
     client.addEventListener('message', (event) => {
         const data = event.detail;
         particle.x = data.x;
         particle.y = data.y;
+        particle.vx = data.vx;
+        particle.vy = data.vy;
     });
 
     app.ticker.add(delta => {
-        const speed = 5;
-        particle.x += controller.move.x * delta * speed;
-        particle.y += controller.move.y * delta * speed;
+        const speed = 0.2;
+        particle.vx += controller.move.x * delta * speed;
+        particle.vy += controller.move.y * delta * speed;
+        particle.x += particle.vx * delta;
+        particle.y += particle.vy * delta;
+
+        // rotate according to acceleration
+        if (controller.move.x !== 0 || controller.move.y !== 0) {
+            particle.rotation = Math.PI / 2 + Math.atan2(controller.move.y, controller.move.x);
+        }
 
         // if controller !== (0,0) then send message
         // so we can get updates from other clients when we are resting
@@ -30,6 +41,8 @@ window.addEventListener('load', () => {
             client.send({
                 x: particle.x,
                 y: particle.y,
+                vx: particle.vx,
+                vy: particle.vy,
             });
         }
 
@@ -37,9 +50,13 @@ window.addEventListener('load', () => {
         if (controller.trigger) {
             particle.x = 0;
             particle.y = 0;
+            particle.vx = 0;
+            particle.vy = 0;
             client.send({
                 x: particle.x,
                 y: particle.y,
+                vx: particle.vx,
+                vy: particle.vy,
             });
         }
     });
